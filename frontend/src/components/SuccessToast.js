7633b455d4c33426f9c21dcdfd737cab7b132b95
Fredrik Johansen <fredrik1206@gmail.com>
2021-07-15 20:30:56 +0200

    Added space between posts and fixed toast

--- frontend/src/components/SuccessToast.js
+++ frontend/src/components/SuccessToast.js
@@ -17,29 +17,35 @@ class SuccessToast extends React.Component {
 	render() {
 		if (this.state.show === true) {
 			return (
-				<div className="absolute bottom-16 right-16">
+				<div className="fixed bottom-16 right-16">
 					<div className="flex bg-white dark:bg-gray-800 w-full mx-auto overflow-hidden rounded-lg shadow-lg">
 						<div className="flex items-center justify-center w-12 bg-green-500 text-white">
-							<FontAwesomeIcon icon={faCheckCircle}></FontAwesomeIcon>
+							<FontAwesomeIcon
+								icon={faCheckCircle}
+							></FontAwesomeIcon>
 						</div>
-		
+
 						<div className="grid grid-rows-2 my-2">
-							<div className="text-green-500 font-semibold ml-3">{this.props.title}</div>
-							<div className="text-gray-500 dark:text-gray-300 ml-3">{this.props.message}</div>
+							<div className="text-green-500 font-semibold ml-3">
+								{this.props.title}
+							</div>
+							<div className="text-gray-500 dark:text-gray-300 ml-3">
+								{this.props.message}
+							</div>
 						</div>
-		
+
 						<div className="flex ml-5 mr-7 text-gray-700 dark:text-white">
 							<button onClick={this.closeToast}>
-								<FontAwesomeIcon icon={faTimes}></FontAwesomeIcon>
+								<FontAwesomeIcon
+									icon={faTimes}
+								></FontAwesomeIcon>
 							</button>
 						</div>
 					</div>
 				</div>
 			);
 		} else {
-			return(
-				<></>
-			);
+			return <></>;
 		}
 	}
 }
